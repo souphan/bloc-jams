080ecc09f61aecf2ae7d4b7026416d344eff9657
Souphan <souphan.ctr@gmail.com>
2015-10-25 08:57:19 -0500

    assignment 29 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
-var pointsArray = document.getElementsByClassName('point');
+/*var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
         point.style.opacity = 1;
@@ -9,40 +9,35 @@ var revealPoint = function(point) {
 
 var animatePoints = function(points) {                    
     forEach(points, revealPoint);
+};*/
+//window.onload = function() {
+ //if (window.innerHeight > 950) {
+         //animatePoints(pointsArray);
+    //var sellingPoints = document.getElementsByClassName('selling-points')[0];//sellingPoints is an array, so we need bracket to choose the first index.
+    //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+       //if (document.body.scrollTop >= scrollDistance) {
+            //animatePoints(pointsArray);
+
+
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-      if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+         animatePoints();
      }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];//sellingPoints is an array, so we need bracket to choose the first index.
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     
-    window.addEventListener('scroll', function(event) {
-       if (document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);    
+$(window).scroll(function(event){
+    if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
        }
 });
-}
-    /* var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-        points[1].style.border = "2px solid pink";
-        points[1].style.msBorder = "2px solid pink";
-        points[1].style.WebkitBorder = "2px solid pink";
-        points[reveal].style.border = "2px solid pink";
-        points[reveal].style.msBorder = "2px solid pink";
-        points[reveal].style.WebkitBorder = "2px solid pink";
-    };
-                    
-     var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-        points[2].style.border = "2px solid pink";
-        points[2].style.msBorder = "2px solid pink";
-        points[2].style.WebkitBorder = "2px solid pink";
-    };*/
+});
